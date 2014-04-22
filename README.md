fc9ea1c976437959f4424e091bfa178b74cf5271
joachimdo <dornbusch@free.fr>
2014-04-22 07:16:06 +0200

    addng composer file with dependencies

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 zf2-queueing-factory-module
 ===========================
+This module only provides an abstract factory in order to give access to the queueing functionalities via zend framework 2 service manager.
--- src/QueueAdapters/Queue/Service/QueueAdaptersAbstractServiceFactory.php
+++ src/QueueAdapters/Queue/Service/QueueAdaptersAbstractServiceFactory.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
+ * Queue Adapter Module for Zend Framework
  *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @link      https://github.com/joachimdo/zf2-queueing-factory-module 
+ * @copyright Copyright (c) 2014 Joachim Dornbusch
+ * @license   GNU GPL V3
  */
 
 namespace QueueAdapters\Queue\Service;
